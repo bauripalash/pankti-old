3f092b9dd9f483dc5770dc2bf2982d91a11518f6
Palash Bauri <palashbauri1@gmail.com>
2022-10-06 18:24:28 +0530

    removed comments made by cobra-cli

--- cmd/repl.go
+++ cmd/repl.go
@@ -20,13 +20,4 @@ var replCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(replCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// replCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// replCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
