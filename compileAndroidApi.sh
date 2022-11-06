2608513a736eb5d7d8481b2cb4b5757a09aed91c
Palash Bauri <palashbauri1@gmail.com>
2022-11-06 21:12:41 +0530

    Fixed typo in Android API compile script

--- compileAndroidApi.sh
+++ compileAndroidApi.sh
@@ -1,2 +1,1 @@
-ANDROID_NDK_HOME=/home/palash/Android/Sdk/ndk/25.1.8937393/ gomobile bind -target=android -androidapi 19 -tags noide bauri.palash/pankti/and
-roidapi
+ANDROID_NDK_HOME=/home/palash/Android/Sdk/ndk/25.1.8937393/ gomobile bind -target=android -androidapi 19 -tags noide bauri.palash/pankti/androidapi
