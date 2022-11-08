b7ba637dbf33dc4257974e9f32eeb2c184b0535b
Palash Bauri <palashbauri1@gmail.com>
2022-11-08 09:42:18 +0530

    java api class now at in.palashbauri.panktijapi

--- compileAndroidApi.sh
+++ compileAndroidApi.sh
@@ -1,1 +1,3 @@
-ANDROID_NDK_HOME=/home/palash/Android/Sdk/ndk/25.1.8937393/ gomobile bind -target=android -androidapi 19 -tags noide bauri.palash/pankti/androidapi
+#!/bin/env sh
+mkdir -p dist
+ANDROID_NDK_HOME=$HOME/Android/Sdk/ndk/25.1.8937393/ gomobile bind -v -o dist/androidapi.aar -target=android -javapkg=in.palashbauri.panktijapi -androidapi 19 -tags noide bauri.palash/pankti/androidapi
