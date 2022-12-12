07d6127e8f8b623e59db6488ab43d1f965ee7b6c
Palash Bauri <palashbauri1@gmail.com>
2022-12-12 21:24:37 +0530

    VERSION 0.2.0

--- compileAndroidApi.sh
+++ compileAndroidApi.sh
@@ -1,5 +1,5 @@
 #!/bin/env sh
 mkdir -p dist
-export PANTKI_VERSION="0.1.1-alpha.1"
+export PANTKI_VERSION="0.2.0"
 ANDROID_NDK_HOME=$HOME/Android/Sdk/ndk/25.1.8937393/ gomobile bind -v -o dist/panktijapi-$PANTKI_VERSION.aar -target=android -javapkg=in.palashbauri.panktijapi -androidapi 19 -tags noide go.cs.palashbauri.in/pankti/androidapi
 cp ./androidapi/androidapi.pom dist/panktijapi-$PANTKI_VERSION.pom
