50bccda5d710f2caefc56aaf482bd7d8bb2927fe
Palash Bauri <palashbauri1@gmail.com>
2022-12-18 20:19:43 +0530

    Write and Create Empty file stdlib

--- stdlib/file.go
+++ stdlib/file.go
@@ -30,6 +30,50 @@ func ReadFile(args []object.Obj) object.Obj {
 	return &object.String{Value: string(d)}
 }
 
+func CreateEmptyFile(args []object.Obj) object.Obj{
+	filename , isOkay := getStringFromArgs(args)
+
+	if !isOkay{
+		return &object.Error{ Msg: "Filename must be string" }
+	}
+
+	if _ , err := os.Stat(filename); err == nil{
+		return &object.Boolean{ Value: false }
+	}
+
+	f , err := os.Create(filename)
+	if err != nil{
+		return &object.Error{ Msg: "Filed to Create File " + filename  }
+	}
+	
+	if err := f.Close(); err == nil{
+		return &object.Error{Msg: "Error while Creating File " + filename + ";" + "Failed to close the file"}
+	}else{
+		return &object.Boolean{Value: true}
+	}
+	
+	
+
+}
+
+func WriteToFile(args []object.Obj) object.Obj{
+	filename , isOkay := getStringFromArgs(args)
+    data := args[1]
+	if !isOkay{
+		return &object.Error{Msg: "Filename must be string"}
+	}
+
+	err := os.WriteFile(filename , []byte(data.Inspect()) , 0644)
+
+	if err != nil{
+		return &object.Error{ Msg: "Failed to write to file " + filename }
+	}
+
+	return &object.Boolean{ Value: true }
+	
+
+}
+
 func FileDirExists(args []object.Obj) object.Obj {
 	result := false
 	filename, isOkay := getStringFromArgs(args)
